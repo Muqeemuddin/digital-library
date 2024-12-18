3f6eb9514829536f40b10e7ac38a096d78a9c05e
Muqeem <muqeemuddin08@gmail.com>
2024-12-18 00:53:12 -0600

    Initial commit

---
+++ src/test/java/com/myminorproject/digitalLibrary/DigitaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myminorproject.digitalLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitaLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
